bf0d9bd6204bfd79aa3cee0030a0b4a37fd7c29f
Prabin <thakurprabin2017@gmail.com>
2021-05-07 20:09:12 +0545

    Initial commit

--- Memory game/index.js
+++ Memory game/index.js
@@ -5,8 +5,10 @@ let game = document.querySelector('.game');
 let front = document.querySelectorAll('.front');
 let boxes = document.querySelectorAll('.boxes');
 let back = document.querySelectorAll('.back');
+let restart = document.querySelector('.restart')
+let restartButton = document.querySelector('.restart_button')
 let timer = 100;
-let flips = 1;
+let flips = 0;
 
 shuffle = () => {
     boxes.forEach(boxes=>{
@@ -15,22 +17,48 @@ shuffle = () => {
     });
 };
 
-setInterval(() => {
-    if(timer >= 0){
-        time.textContent = timer--
-    };
-}, 1000);
-
 start.addEventListener('click',()=>{
     start.style.display = 'none';
     game.style.display = 'inline';
     shuffle();
+    setInterval(() => {
+        if(timer >= 0){
+            time.textContent = timer--
+        }
+        if(timer < 0){
+            game.style.display = 'none';
+            restart.style.display = 'inline';
+            timer = 100;
+        };
+    }, 1000);    
 });
 
 boxes.forEach(boxes => {
     boxes.addEventListener('click',()=> {
-        boxes.classList.toggle("rotate");
-        flip.textContent = flips++;
+        boxes.classList.add("rotate");
         console.log(boxes.dataset.name);
+        setTimeout(()=> {
+            boxes.classList.remove("rotate")
+        },1500);
+    });
+});
+
+restartButton.addEventListener('click',()=>{
+    start.style.display = 'none';
+    game.style.display = 'inline';
+    restart.style.display = 'none';
+    shuffle();
+    timer = 100;
+    flips = 0;
+    flip.textContent = flips;
+    boxes.forEach(boxes => {
+        boxes.classList.remove("rotate");
+    });
+});
+
+front.forEach(front =>{
+    front.addEventListener('click',()=>{
+        flips++;
+        flip.textContent = flips;
     });
 });
