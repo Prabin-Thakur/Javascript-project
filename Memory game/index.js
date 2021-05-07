8320ddbb813f78f06ac99b8f3678a789f47a2462
Prabin <thakurprabin2017@gmail.com>
2021-05-07 13:34:02 +0545

    Initial commit

--- Memory game/index.js
+++ Memory game/index.js
@@ -1,18 +1,41 @@
 let time = document.querySelector('.time');
+let flip = document.querySelector('.flip');
 let start = document.querySelector('.start_button');
-let game = document.querySelector('.game')
+let game = document.querySelector('.game');
+let front = document.querySelectorAll('.front');
 let boxes = document.querySelectorAll('.boxes');
+let back = document.querySelectorAll('.back');
 let timer = 100;
+let flips = 1;
 
-flip = () =>{
-    
-}
+shuffle = () => {
+    boxes.forEach(boxes=>{
+        let a = Math.floor(Math.random()* 20);
+        boxes.style.order = a;
+    });
+};
+
+setInterval(() => {
+    if(timer >= 0){
+        time.textContent = timer--
+    };
+}, 1000);
 
 start.addEventListener('click',()=>{
     start.style.display = 'none';
     game.style.display = 'inline';
+    shuffle();
 });
 
 boxes.forEach(boxes => {
-    boxes.addEventListener('click',()=> flip);
+    boxes.addEventListener('click',()=> {
+        boxes.classList.toggle("rotate");
+        flip.textContent = flips++;
+    });
+});
+
+front.forEach(front => {
+front.addEventListener('click',()=> {
+       console.log(front.getAttribute('src'))
+    });
 });
